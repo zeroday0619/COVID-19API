b16bb920b7314eea635ff69c5a4f0f91b2e4887d
zeroday0619 <zeroday0619@kakao.com>
2020-03-09 19:01:04 +0900

    json 구조 변경 | 최적화

--- app/ext/KrStatusRegion.py
+++ app/ext/KrStatusRegion.py
@@ -2,16 +2,16 @@ import ujson
 from app.crawler.mohw import GetInfectiousDiseasesbyRegion
 
 async def KrStatusRegion(cache, loop):
-	if not await cache.exists('idr'):
+	if not await cache.exists('region'):
 		data = GetInfectiousDiseasesbyRegion()
 		result = await data.AllRegion()
 		jsondata = {
-			"idr": result
+			"region": result
 		}
 		ob = await loop.run_in_threadpool(lambda: ujson.dumps(jsondata).encode('utf-8'))
 		#await cache.set('idr', ob, expire=3600)
 		return jsondata
 	else:
-		abc = await cache.get('idr', encoding='utf-8')
+		abc = await cache.get('region', encoding='utf-8')
 		adad = await loop.run_in_threadpool(lambda: ujson.loads(abc))
 		return adad
--- app/ext/krstatus.py
+++ app/ext/krstatus.py
@@ -2,16 +2,16 @@ from ..crawler.mohw import InfectiousDiseases
 import ujson
 
 async def krstatus(cache, loop):
-	if not await cache.exists('info'):
+	if not await cache.exists('krstatus'):
 		data = InfectiousDiseases()
 		Result = await data.Convert()
 		rs = {
-			'info': Result
+			'krstatus': Result
 		}
 		pb = await loop.run_in_threadpool(lambda: ujson.dumps(rs).encode('utf-8'))
-		await cache.set('info', pb, expire=3600)
+		await cache.set('krstatus', pb, expire=3600)
 		return rs
 	else:
-		abc = await cache.get('info', encoding='utf-8')
+		abc = await cache.get('krstatus', encoding='utf-8')
 		adad = await loop.run_in_threadpool(lambda: ujson.loads(abc))
 		return adad
