e9001c3bb95bb914e26e3ca1d8a5e97ab7171a7f
zeroday0619 <zeroday0619@kakao.com>
2020-03-12 12:04:48 +0900

    Disable Debug Mode

--- run.py
+++ run.py
@@ -38,8 +38,8 @@ class AppSettings(OtherSettings, fastapi_plugins.RedisSettings):
 app = FastAPI(
 	title="COVID-19 API",
 	description="## 코로나 바이러스 감염증 -19 (COVID-19)의 국내 현황/뉴스 제공 API \n\n ### Project Repo: [Github](https://github.com/zeroday0619/COVID-19API)",
-	version="2020.03.11 B1",
-	debug=True
+	version="2020.03.12 B2",
+	debug=False
 )
 config = AppSettings()
 
