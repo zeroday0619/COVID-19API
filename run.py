3bdea436e3f6704766eaeb85babddc6f8ff027de
Euiseo Cha <zeroday0619_dev@outlook.com>
2021-08-14 10:21:37 +0900

    feat: update description

--- run.py
+++ run.py
@@ -3,7 +3,7 @@
 :contact: zeroday0619(at)kakao.com
 :copyright: Copyright 2020, zeroday0619
 """
-__version__ = "Ver 3.0"
+__version__ = "Ver 3.1"
 
 import uvicorn
 from fastapi import FastAPI
@@ -13,7 +13,7 @@ from starlette.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title="COVID-19 API",
-    description="## 코로나 바이러스 감염증 -19 (COVID-19)의 국내/해외 현황/뉴스 제공 API \n\n ### Project Repo: [Github]("
+    description="## 코로나바이러스감염증-19 (COVID-19) API \n\n ### Project Repo: [Github]("
                 "https://github.com/zeroday0619/COVID-19API)",
     version=__version__,
     debug=False
